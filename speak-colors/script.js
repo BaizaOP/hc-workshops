d447cb9a65a471a4048a33d55993888ad6bae7fa
BaizaOP <zayn.baig@gmail.com>
2022-02-04 13:12:07 -0500

    Added Speak Colors workshop

---
+++ speak-colors/script.js
@@ -0,0 +1,50 @@
+// TODO: if there is a color word AT ALL in the sentence, display the color
+let prevOKColor = "white";
+                                         // call back func we can use!
+const speech = new p5.SpeechRec('en-US', () => {
+  if(speech.resultValue) {
+    const rawText = speech.resultString.toUpperCase();
+    const color = rawText.split(' ').join('');
+    if(isColor(color)) { 
+      background(color);
+      prevOKColor = color;
+      text(color, width/2, height/2);
+    } else {
+      background(prevOKColor);
+      text(rawText, width/2, height/2);
+    }
+    console.log(color, text);
+  }
+});
+
+// speech api stuffs
+speech.continuous = true;
+speech.interimResults = false;
+
+// setup initial screen
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  fill(25);
+
+  textSize(48);
+  textAlign(CENTER);
+  textStyle(BOLDITALIC);
+  textFont('"Avenir Next", system-ui, sans-serif');
+  text('SAY A COLOR!', width/2, height/2);
+  
+  // start the speech api
+  speech.start();
+}
+
+function draw() {
+  /* dont really need this */
+}
+
+// function to determine if a word is a color,
+// only change screen color if valid color
+function isColor(strColor) {
+  let s = new Option().style;
+  s.color = strColor;
+  return s.color !== '';
+}
