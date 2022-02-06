08a867fd740ce8db2e8bca570f58035f19464a32
BaizaOP <zayn.baig@gmail.com>
2022-02-05 21:29:32 -0500

    added Sound Galaxy Workshop!

---
+++ sound-galaxy/README.md
@@ -0,0 +1,5 @@
+# Sound Galaxy
+
+-=Speak and see the magic=-
+
+To run, follow the instructions in the parent directory
---
+++ sound-galaxy/script.js
@@ -0,0 +1,78 @@
+// TODO: Doesn't seem to work on reload, debug?
+
+let fft; // p5.js Fast Fourier Transform
+let galaxy;
+
+let w, h;
+
+function setup() {
+  w = windowWidth;
+  h = windowHeight;
+
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+
+  let mic = new p5.AudioIn();
+  mic.start();
+
+  fft = new p5.FFT();
+  fft.setInput(mic);
+  galaxy = new Galaxy(1024);
+  galaxy.positionParticles();
+  galaxy.drawParticles();
+}
+
+function draw() {
+  background(0,0,0,60);
+  galaxy.drawParticles();
+  let spectrum = fft.analyze();
+  galaxy.updateParticles(spectrum);
+
+}
+
+class Particle {
+  constructor (position) {
+    this.position = position;
+    this.speed = createVector(0, 1);
+    this.diameter = 10;
+    this.r = random(0,255);
+    this.g = random(0,255);
+    this.b = random(0,255);
+    this.color = [this.r, this.g, this.b, 100];
+  }
+  draw() {
+    circle(this.position.x, this.position.y, this.diameter);
+    fill(this.color);
+  }
+  update(energy) {
+    this.diameter = random(5, 7) + energy / 7.5;
+    this.position.y += this.speed.y * energy / 30;
+    if(this.position.y > h) {
+      this.position.y = 0;
+    }
+  }
+}
+
+class Galaxy {
+  constructor (num) {
+    this.num = num; 
+    this.particles = [];
+  }
+  positionParticles () {
+    for (let i = 0; i < this.num; i++) {
+      const particle = new Particle (createVector(Math.round(Math.random() * w), 
+                                                  Math.round(Math.random() * h)));
+      this.particles.push(particle);
+    }
+  }
+  drawParticles() {
+    for(let i = 0; i < this.particles.length; i++) {
+      this.particles[i].draw();
+    }
+  }
+  updateParticles(energy) {
+    for(let i = 0; i < this.particles.length; i++) {
+      this.particles[i].update(energy[i % 248]);
+    }
+  }
+}
