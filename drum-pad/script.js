5e0c925ac3ca12b7080f5f7d737dd7c317f1d047
BaizaOP <zayn.baig@gmail.com>
2022-02-12 19:25:16 -0500

    Added Drum Pad Workshop!

---
+++ drum-pad/script.js
@@ -0,0 +1,52 @@
+// Put Javascript logic into here!
+
+// figure out how to make links an array and programmatically add event listeners to each box?
+
+const pad = document.querySelector(".pad");
+const muzak = [
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/SD0025.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/SD0010.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/SD0000.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/RS.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/OH25.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/MA.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CY0010.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CH.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CB.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/BD0010.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/BD0000.mp3',
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/CP.mp3',
+];
+
+for(let i = 0; i < pad.children.length; i++) {
+  pad.children[i].addEventListener("click", () => {
+    play(muzak[i]);
+  });
+}
+
+function play(link) {
+  let audio = new Audio(link);
+  audio.load();
+  audio.play();
+}
+
+// got this addEvent from: https://stackoverflow.com/questions/16089421/simplest-way-to-detect-keypresses-in-javascript
+addEvent(document, "keypress", function (e) {
+  e = e || window.event;
+  if(e.keyCode >= "A".charCodeAt() && e.keyCode <= "L".charCodeAt()) {
+    play(muzak[e.keyCode - "A".charCodeAt()]); 
+  } else if (e.keyCode >= "a".charCodeAt() && e.keyCode <= "l".charCodeAt()) {
+    play(muzak[e.keyCode - "a".charCodeAt()]); 
+  }
+  
+});
+
+function addEvent(element, eventName, callback) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, callback, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + eventName, callback);
+    } else {
+        element["on" + eventName] = callback;
+    }
+}
